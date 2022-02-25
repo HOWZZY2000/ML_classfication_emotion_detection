5c667460d0fbb624d22ce68ee92b3e222f548c83
Gary <gary.zhou@ontario.ca>
2022-02-24 21:49:40 -0500

    feature selection

--- SVM.py
+++ SVM.py
@@ -2,17 +2,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from Settings import *
-from Preprocessing import Preprocessor
+from Feature_selection import *
 
-
-# print(df.iloc[:, 1:].head(5))
-
-# print(df.iloc[:, 0].head(5))
 data = Preprocessor()
 data.missing_value_handling()
+# data.df.loc[:, importance_feature_selection(data)]
 
-X_train, X_test, Y_train, Y_test = train_test_split(data.df.iloc[:, 1:], data.df.iloc[:, 0], test_size=0.3)
+X_train, X_test, Y_train, Y_test = train_test_split(data.df.iloc[:, 1:], data.df.iloc[:, 0], test_size=0.4)
 clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
 
 clf.fit(X_train, Y_train)
--- Settings.py
+++ Settings.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-# save global constant paths
+# Save global constant paths
 data_folder = Path("dataset/")
 ECG = data_folder / "ECG_FeaturesExtracted.csv"
 ET = data_folder / "EyeTracking_FeaturesExtracted.csv"
 GSR = data_folder / "GSR_FeaturesExtracted.csv"
 
-# For printing purpose
+# Only for printing purpose
 pd.set_option("display.max_rows", None, "display.max_columns", None)
