05e18bcb91526f2eced5f2eedb63c9ca4d5cad43
Gary <gary.zhou@ontario.ca>
2022-02-25 14:48:55 -0500

    more feature

--- Preprocessing/Preprocessing.py
+++ Preprocessing/Preprocessing.py
@@ -11,6 +11,9 @@ class Preprocessor:
         df = pd.concat(map(pd.read_csv, path), axis=1) # combining all data files
         self.df = df.loc[:, ~df.columns.duplicated()] # remove duplicated columns
 
+    def exam(self) -> None:
+        return
+
     def missing_value_handling(self, index=1) -> None:
         if self.df.isna().values.any():
             if index == 0:  # drop the row
@@ -21,11 +24,11 @@ class Preprocessor:
             elif index == 2:  # using median
                 for i in self.df.columns[self.df.isna().any()].tolist():
                     self.df[i] = self.df[i].replace(np.NaN, self.df[i].median())
-            elif index == 3:  # using 0
-                self.df.fillna(0)
-            elif index == 4: # using linear regression
+            elif index == 3:  # using mode
                 for i in self.df.columns[self.df.isna().any()].tolist():
-                    test = self.df[i].isna()
+                    self.df[i] = self.df[i].replace(np.NaN, self.df[i].mode())
+            elif index == 4: # using 0
+                self.df.fillna(0)
             else:
                 raise ValueError('index does not exist')
 
